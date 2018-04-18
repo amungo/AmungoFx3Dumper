a69c10597b6f4b79eccf521a770143482f95ace3
Ivan Makushkin <mivcamry@gmail.com>
2018-04-18 12:51:46 +0300

    Add SPI commands for nut2nt
    Add SPI commands for nut2nt

--- src/hwfx3/host_commands.h
+++ src/hwfx3/host_commands.h
@@ -8,6 +8,13 @@
 #define CMD_REG_READ        ( 0xB5 )
 #define CMD_CYPRESS_RESET   ( 0xBF )
 
+#define CMD_ECP5_WRITE      ( 0xD1 )
+#define CMD_ECP5_READ       ( 0xD5 )
+#define CMD_ECP5_CSON       ( 0xD3 )
+#define CMD_ECP5_CSOFF      ( 0xD4 )
+#define CMD_ECP5_CHECK      ( 0xD2 )
+
+
 typedef struct FirmwareDescription_t {
 	uint32_t version;
 	uint8_t  reserved[ 28 ];
