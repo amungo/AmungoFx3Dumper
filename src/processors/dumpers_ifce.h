05eab91f63c6d514af092284880b019be149a649
Amungo <amungo.navigation@gmail.com>
2018-07-19 12:05:51 +0300

    [.] +DumperIfce

---
+++ src/processors/dumpers_ifce.h
@@ -0,0 +1,18 @@
+#ifndef DUMPERIFCE_H
+#define DUMPERIFCE_H
+
+#include <cstdint>
+
+class DumperIfce {
+public:
+    virtual ~DumperIfce() = default;
+    struct DumpInfo_t {
+        int error_count = 0;
+        int64_t bytes_dumped = 0;
+    };
+
+    virtual DumpInfo_t GetInfo() = 0;
+};
+
+
+#endif
